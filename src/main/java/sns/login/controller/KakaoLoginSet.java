ef2f5615f84a26bcfe27ac1e1410c51d84dd81d0
hyunbong jang <wkdgusqhd080@naver.com>
2020-04-26 17:32:15 +0900

    pwd del

--- src/main/java/sns/login/controller/KakaoLoginSet.java
+++ src/main/java/sns/login/controller/KakaoLoginSet.java
@@ -1,7 +1,7 @@
 package sns.login.controller;
 
 public class KakaoLoginSet {
-	public final static String CLIENT_ID = "ecf1dec6a59adf5029bdc3a56575c61c";
+	public final static String CLIENT_ID = "";
 	public final static String REDIRECT_URI = "http://127.0.0.1:8000/kakaoLogin/login.do";
 	
 }
